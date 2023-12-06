bcaaba0442c915e88c59732320917b137fee11c9
Vitor-Silva27 <developer.vitorsilva@gmail.com>
2023-12-06 12:21:45 -0300

    feat: finish mobile home estilization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,22 @@ import { SimpleHeader } from '@/components/SimpleHeader';
 import styles from './page.module.css'
 import { SearchBar } from '@/components/SearchBar';
 import { CardButton } from '@/components/CardButton';
+import { CardButtonsContainer } from '@/components/CardButtonsContainer';
 
 export default function Home() {
   return (
-    <>
+    <div className={styles.homeContainer}>
       <SimpleHeader />
       <h1 className={`title ${styles.mainTitle}`}>Explore os setores do campus</h1>
       <SearchBar text='Pesquisar...'/>
-      <CardButton link='https://www.google.com' title='titulo' icon='assets/clock-light.svg'/>
-    </>
+      <div className={styles.mainCardsContainer}>
+        <CardButtonsContainer>
+          <CardButton link='https://www.google.com' title='Setores' icon='assets/hat.svg' main/>
+          <CardButton link='https://www.google.com' title='Horários' icon='assets/clock-light.svg'/>
+          <CardButton link='https://www.google.com' title='Mapa' icon='assets/map-light.svg'/>
+          <CardButton link='https://www.google.com' title='Protocolos' icon='assets/doc-light.svg'/>
+        </CardButtonsContainer>
+      </div>
+    </div>
   );
 }
---
+++ src/components/CardButtonsContainer/index.ts
@@ -0,0 +1,1 @@
+export { CardButtonsContainer } from "./CardButtonsContainer";
