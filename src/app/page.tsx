8f26050a99eba6fd79cebf5a79a829e843b769f4
Vitor-Silva27 <developer.vitorsilva@gmail.com>
2023-12-06 11:47:11 -0300

    feat: create searchbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
 import { SimpleHeader } from '@/components/SimpleHeader';
 import styles from './page.module.css'
+import { SearchBar } from '@/components/SearchBar';
 
 export default function Home() {
   return (
-    <SimpleHeader />
+    <>
+      <SimpleHeader />
+      <h1 className={`title ${styles.mainTitle}`}>Explore os setores do campus</h1>
+      <SearchBar text='Pesquisar...'/>
+    </>
   );
 }
---
+++ src/components/SearchBar/SearchBar.tsx
@@ -0,0 +1,15 @@
+import Image from 'next/image';
+import style from './style.module.css';
+
+type SearchBarProps = {
+  text: string;
+}
+
+export const SearchBar = ({text}: SearchBarProps) => {
+  return (
+    <div className={style.searchContainer}>
+      <input className={style.searchInput} type='search' placeholder={text} />
+      <div className={style.searchButton}><Image src="assets/icone-pesquisa.svg" alt='search icon' width={24} height={24} /></div>
+    </div>
+  );
+}
