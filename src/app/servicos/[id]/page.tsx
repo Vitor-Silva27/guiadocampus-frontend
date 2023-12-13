a6fa7723479d4f8cc2e945baf413f29c6e1cac28
Vitor-Silva27 <developer.vitorsilva@gmail.com>
2023-12-13 15:27:19 -0300

    fix: loading service

--- src/app/servicos/[id]/page.tsx
+++ src/app/servicos/[id]/page.tsx
@@ -4,27 +4,30 @@ import { IService } from "@/services/api/types/IService";
 import { useFetch } from "@/services/useFetch";
 import { ServiceDescription } from "./components/description";
 import styles from "./page.module.css";
-import { HeaderWithReturn, CardButton } from "@/components";
+import { HeaderWithReturn, CardButton, Loading } from "@/components";
 
 
 export default function Service({ params }: { params: { id: string } }) {
-  const {data} = useFetch<IService>(`procedures/${params.id}`);
+  const {data, loading} = useFetch<IService>(`procedures/${params.id}`);
     return (
       <>
        <HeaderWithReturn />
-      {data && (
-        <main>
-          <h1 className={`title ${styles.serviceTitle}`}>{data.title}</h1>
-          <ServiceDescription text={data.description}/>
-          <section className={`${styles.section} ${styles.cardsSection}`}>
-          {
-            data.embeds.map(embed => (
-              <CardButton key={embed.id} title={`ver ${embed.title}`} icon={embed.icon} link={embed.link} main/>
-            ))
-          }
-        </section>
-        </main>
-      )}
+       {loading ? <Loading />:(<>
+        {data && (
+          <main>
+            <h1 className={`title ${styles.serviceTitle}`}>{data.title}</h1>
+            <ServiceDescription text={data.description}/>
+            <section className={`${styles.section} ${styles.cardsSection}`}>
+            {
+              data.embeds.map(embed => (
+                <CardButton key={embed.id} title={`ver ${embed.title}`} icon={embed.icon} link={embed.link} main/>
+              ))
+            }
+          </section>
+          </main>
+        )}
+       </>)
+       }
 
       </>
     );
